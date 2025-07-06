53dafe482115ab3e2c48224bb7da1ed2020740c1
nimiCurtis <nimicu21@gmail.com>
2025-07-07 01:39:33 +0300

    apply uncrustify

--- so101_teleop/src/keyboard_servo_node.cpp
+++ so101_teleop/src/keyboard_servo_node.cpp
@@ -25,10 +25,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include "so101_teleop/keyboard_servo_component.hpp"
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<moveit_servo::KeyboardServoComponent>(rclcpp::NodeOptions()));
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<moveit_servo::KeyboardServoComponent>(rclcpp::NodeOptions()));
+  rclcpp::shutdown();
+  return 0;
 }
