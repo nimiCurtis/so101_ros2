c453614e36936948b762e5e6d2b61fd06bd5bd16
nimiCurtis <nimicu21@gmail.com>
2025-07-29 19:08:24 +0300

    add usb_cam

---
+++ so101_bringup/launch/include/camera.launch.py
@@ -0,0 +1,108 @@
+# Copyright 2025 nimiCurtis
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+import os
+from pathlib import Path
+from typing import List, Optional
+
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch_ros.actions import Node
+
+USB_CAM_DIR = get_package_share_directory('so101_bringup')
+
+
+def load_yaml(package_name: str, file_path: str):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError as e:
+        print(f"Failed to load {absolute_file_path}: {e}")
+        return None
+
+
+class CameraConfig:
+    def __init__(
+        self,
+        name: str,
+        param_path: Path,
+        remappings: Optional[List] = None,
+        namespace: Optional[str] = None,
+    ):
+        self.name = name
+        self.namespace = namespace
+        self.param_path = param_path
+        self._validate_param_path()
+
+        if remappings is None:
+            remappings = [
+                ('image_raw', f'{name}/image_raw'),
+                ('image_raw/compressed', f'{name}/image_compressed'),
+                ('image_raw/compressedDepth', f'{name}/compressedDepth'),
+                ('image_raw/theora', f'{name}/image_raw/theora'),
+                ('camera_info', f'{name}/camera_info'),
+            ]
+        self.remappings = remappings
+
+    def _validate_param_path(self):
+        if not self.param_path.exists():
+            raise FileNotFoundError(f'Could not find parameter file: {self.param_path}')
+
+
+def get_camera_configs(
+    config_file: str = 'config/so101_cameras.yaml',
+) -> List[CameraConfig]:
+    yaml_config = load_yaml('so101_bringup', config_file)
+    cameras: List[CameraConfig] = []
+
+    if yaml_config and 'cameras' in yaml_config:
+        for cam in yaml_config['cameras']:
+            name = cam.get('name')
+            relative_param_path = cam.get('param_path')
+            full_param_path = Path(USB_CAM_DIR, 'config', relative_param_path)
+            cameras.append(CameraConfig(name=name, param_path=full_param_path))
+    else:
+        raise RuntimeError(f"No valid 'cameras' list found in {config_file}")
+
+    return cameras
+
+
+def generate_launch_description():
+    camera_configs = get_camera_configs()
+
+    camera_nodes = [
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            output='screen',
+            name=camera.name,
+            namespace=camera.namespace,
+            parameters=[camera.param_path],
+            remappings=camera.remappings,
+        )
+        for camera in camera_configs
+    ]
+
+    return LaunchDescription([GroupAction(camera_nodes)])
--- so101_teleop/src/joystick_servo_component.cpp
+++ so101_teleop/src/joystick_servo_component.cpp
@@ -20,28 +20,28 @@ const std::string BASE_FRAME_ID = "base_link";
 // For XBOX 1 controller
 enum Axis
 {
-    LEFT_STICK_X = 0,
-    LEFT_STICK_Y = 1,
-    LEFT_TRIGGER = 2,
-    RIGHT_STICK_X = 3,
-    RIGHT_STICK_Y = 4,
-    RIGHT_TRIGGER = 5,
-    D_PAD_X = 6,
-    D_PAD_Y = 7
+  LEFT_STICK_X = 0,
+  LEFT_STICK_Y = 1,
+  LEFT_TRIGGER = 2,
+  RIGHT_STICK_X = 3,
+  RIGHT_STICK_Y = 4,
+  RIGHT_TRIGGER = 5,
+  D_PAD_X = 6,
+  D_PAD_Y = 7
 };
 enum Button
 {
-    A = 0,
-    B = 1,
-    X = 2,
-    Y = 3,
-    LEFT_BUMPER = 4,
-    RIGHT_BUMPER = 5,
-    CHANGE_VIEW = 6,
-    MENU = 7,
-    HOME = 8,
-    LEFT_STICK_CLICK = 9,
-    RIGHT_STICK_CLICK = 10
+  A = 0,
+  B = 1,
+  X = 2,
+  Y = 3,
+  LEFT_BUMPER = 4,
+  RIGHT_BUMPER = 5,
+  CHANGE_VIEW = 6,
+  MENU = 7,
+  HOME = 8,
+  LEFT_STICK_CLICK = 9,
+  RIGHT_STICK_CLICK = 10
 };
 
 // Some axes have offsets (e.g. the default trigger position is 1.0 not 0)
@@ -59,44 +59,43 @@ std::map<Button, double> BUTTON_DEFAULTS;
  * @return return true if you want to publish a Twist, false if you want to publish a JointJog
  */
 bool convertJoyToCmd(
-    const std::vector<float> &axes, const std::vector<int> &buttons,
-    std::unique_ptr<geometry_msgs::msg::TwistStamped> &twist,
-    std::unique_ptr<control_msgs::msg::JointJog> &joint)
+  const std::vector<float> & axes, const std::vector<int> & buttons,
+  std::unique_ptr<geometry_msgs::msg::TwistStamped> & twist,
+  std::unique_ptr<control_msgs::msg::JointJog> & joint)
 {
-    // Give joint jogging priority because it is only buttons
-    // If any joint jog command is requested, we are only publishing joint commands
-    if (buttons[A] || buttons[B] || buttons[X] || buttons[Y] || axes[D_PAD_X] || axes[D_PAD_Y])
-    {
-        // Map the D_PAD to the proximal joints
-        joint->joint_names.push_back("shoulder_pan");
-        joint->velocities.push_back(axes[D_PAD_X]);
-        joint->joint_names.push_back("shoulder_lift");
-        joint->velocities.push_back(axes[D_PAD_Y]);
+  // Give joint jogging priority because it is only buttons
+  // If any joint jog command is requested, we are only publishing joint commands
+  if (buttons[A] || buttons[B] || buttons[X] || buttons[Y] || axes[D_PAD_X] || axes[D_PAD_Y]) {
+    // Map the D_PAD to the proximal joints
+    joint->joint_names.push_back("shoulder_pan");
+    joint->velocities.push_back(axes[D_PAD_X]);
+    joint->joint_names.push_back("shoulder_lift");
+    joint->velocities.push_back(axes[D_PAD_Y]);
 
-        // Map the diamond to the distal joints
-        joint->joint_names.push_back("elbow_flex");
-        joint->velocities.push_back(buttons[B] - buttons[X]);
-        joint->joint_names.push_back("wrist_flex");
-        joint->velocities.push_back(buttons[Y] - buttons[A]);
-        joint->joint_names.push_back("wrist_roll");
-        joint->velocities.push_back(0.);
-        return false;
-    }
-    twist->twist.linear.z = axes[LEFT_STICK_Y];
-    twist->twist.linear.x = axes[LEFT_STICK_X];
+    // Map the diamond to the distal joints
+    joint->joint_names.push_back("elbow_flex");
+    joint->velocities.push_back(buttons[B] - buttons[X]);
+    joint->joint_names.push_back("wrist_flex");
+    joint->velocities.push_back(buttons[Y] - buttons[A]);
+    joint->joint_names.push_back("wrist_roll");
+    joint->velocities.push_back(0.);
+    return false;
+  }
+  twist->twist.linear.z = axes[LEFT_STICK_Y];
+  twist->twist.linear.x = axes[LEFT_STICK_X];
 
-    double lin_y_right = 0.5 * (axes[RIGHT_TRIGGER] - AXIS_DEFAULTS.at(RIGHT_TRIGGER));
-    double lin_y_left = -0.5 * (axes[LEFT_TRIGGER] - AXIS_DEFAULTS.at(LEFT_TRIGGER));
-    twist->twist.linear.y = lin_y_right + lin_y_left;
+  double lin_y_right = 0.5 * (axes[RIGHT_TRIGGER] - AXIS_DEFAULTS.at(RIGHT_TRIGGER));
+  double lin_y_left = -0.5 * (axes[LEFT_TRIGGER] - AXIS_DEFAULTS.at(LEFT_TRIGGER));
+  twist->twist.linear.y = lin_y_right + lin_y_left;
 
-    twist->twist.angular.y = axes[RIGHT_STICK_X];
-    twist->twist.angular.x = axes[RIGHT_STICK_Y];
+  twist->twist.angular.y = axes[RIGHT_STICK_X];
+  twist->twist.angular.x = axes[RIGHT_STICK_Y];
 
-    double roll_positive = -1 * buttons[RIGHT_BUMPER];
-    double roll_negative = (buttons[LEFT_BUMPER]);
-    twist->twist.angular.z = roll_positive + roll_negative;
+  double roll_positive = -1 * buttons[RIGHT_BUMPER];
+  double roll_negative = (buttons[LEFT_BUMPER]);
+  twist->twist.angular.z = roll_positive + roll_negative;
 
-    return true;
+  return true;
 }
 
 /** \brief // This should update the frame_to_publish_ as needed for changing command frame via
@@ -104,90 +103,87 @@ bool convertJoyToCmd(
  * @param frame_name Set the command frame to this
  * @param buttons The vector of discrete controller button values
  */
-void updateCmdFrame(std::string &frame_name, const std::vector<int> &buttons)
+void updateCmdFrame(std::string & frame_name, const std::vector<int> & buttons)
 {
-    if (buttons.size() > CHANGE_VIEW && buttons[CHANGE_VIEW] && frame_name == EEF_FRAME_ID)
-    {
-        frame_name = BASE_FRAME_ID;
-        RCLCPP_INFO(rclcpp::get_logger("joy_teleop"), "Changed frame to BASE_FRAME_ID");
-    }
-    else if (buttons.size() > MENU && buttons[MENU] && frame_name == BASE_FRAME_ID)
-    {
-        frame_name = EEF_FRAME_ID;
-        RCLCPP_INFO(rclcpp::get_logger("joy_teleop"), "Changed frame to EEF_FRAME_ID");
-    }
+  if (buttons.size() > CHANGE_VIEW && buttons[CHANGE_VIEW] && frame_name == EEF_FRAME_ID) {
+    frame_name = BASE_FRAME_ID;
+    RCLCPP_INFO(rclcpp::get_logger("joy_teleop"), "Changed frame to BASE_FRAME_ID");
+  } else if (buttons.size() > MENU && buttons[MENU] && frame_name == BASE_FRAME_ID) {
+    frame_name = EEF_FRAME_ID;
+    RCLCPP_INFO(rclcpp::get_logger("joy_teleop"), "Changed frame to EEF_FRAME_ID");
+  }
 }
 
 namespace moveit_servo
 {
 
-    JoyToServoComponent::JoyToServoComponent(const rclcpp::NodeOptions &options)
-        : Node("so101_joy_teleop", options),
-          frame_to_publish_(BASE_FRAME_ID),
-          joint_vel_cmd_(0.0),
-          running_(true)
-    {
-        RCLCPP_INFO(this->get_logger(), "Starting JoyToServoComponent...");
+JoyToServoComponent::JoyToServoComponent(const rclcpp::NodeOptions & options)
+: Node("so101_joy_teleop", options),
+  frame_to_publish_(BASE_FRAME_ID),
+  joint_vel_cmd_(0.0),
+  running_(true)
+{
+  RCLCPP_INFO(this->get_logger(), "Starting JoyToServoComponent...");
 
-        joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-            JOY_TOPIC, rclcpp::SystemDefaultsQoS(),
-            [this](const sensor_msgs::msg::Joy::ConstSharedPtr &msg)
-            { return JoyCBLoop(msg); });
+  joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    JOY_TOPIC, rclcpp::SystemDefaultsQoS(),
+    [this](const sensor_msgs::msg::Joy::ConstSharedPtr & msg)
+    {return JoyCBLoop(msg);});
 
-        twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(TWIST_TOPIC, rclcpp::SystemDefaultsQoS());
-        joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>(JOINT_TOPIC, rclcpp::SystemDefaultsQoS());
+  twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(
+    TWIST_TOPIC,
+    rclcpp::SystemDefaultsQoS());
+  joint_pub_ = this->create_publisher<control_msgs::msg::JointJog>(
+    JOINT_TOPIC,
+    rclcpp::SystemDefaultsQoS());
 
-        servo_start_client_ = this->create_client<std_srvs::srv::Trigger>("/servo_node/start_servo");
-        if (!servo_start_client_->wait_for_service(std::chrono::seconds(3)))
-        {
-            RCLCPP_WARN(this->get_logger(), "Servo start service not available after 3 second.");
-        }
-        else
-        {
-            RCLCPP_INFO(this->get_logger(), "Calling start_servo service...");
-            servo_start_client_->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
-        }
-    }
+  servo_start_client_ = this->create_client<std_srvs::srv::Trigger>("/servo_node/start_servo");
+  if (!servo_start_client_->wait_for_service(std::chrono::seconds(3))) {
+    RCLCPP_WARN(this->get_logger(), "Servo start service not available after 3 second.");
+  } else {
+    RCLCPP_INFO(this->get_logger(), "Calling start_servo service...");
+    servo_start_client_->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
+  }
+}
 
-    /**
+/**
      * @brief Destructor for JoyToServoComponent.
      * Sets the running flag to false and joins the joystick reading thread to ensure proper shutdown.
      */
-    JoyToServoComponent::~JoyToServoComponent() {}
+JoyToServoComponent::~JoyToServoComponent() {}
 
-    void JoyToServoComponent::JoyCBLoop(const sensor_msgs::msg::Joy::ConstSharedPtr &msg)
-    {
-        RCLCPP_DEBUG(this->get_logger(), "Received joystick input.");
+void JoyToServoComponent::JoyCBLoop(const sensor_msgs::msg::Joy::ConstSharedPtr & msg)
+{
+  RCLCPP_DEBUG(this->get_logger(), "Received joystick input.");
 
-        auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
-        auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
+  auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+  auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
 
-        // This call updates the frame for twist commands
-        updateCmdFrame(frame_to_publish_, msg->buttons);
+  // This call updates the frame for twist commands
+  updateCmdFrame(frame_to_publish_, msg->buttons);
 
-        if (convertJoyToCmd(msg->axes, msg->buttons, twist_msg, joint_msg))
-        {
-            RCLCPP_INFO(this->get_logger(), "Publishing twist command: lin[x,y,z]=[%.2f, %.2f, %.2f], ang[x,y,z]=[%.2f, %.2f, %.2f]",
-                        twist_msg->twist.linear.x, twist_msg->twist.linear.y, twist_msg->twist.linear.z,
-                        twist_msg->twist.angular.x, twist_msg->twist.angular.y, twist_msg->twist.angular.z);
+  if (convertJoyToCmd(msg->axes, msg->buttons, twist_msg, joint_msg)) {
+    RCLCPP_INFO(
+      this->get_logger(), "Publishing twist command: lin[x,y,z]=[%.2f, %.2f, %.2f], ang[x,y,z]=[%.2f, %.2f, %.2f]",
+      twist_msg->twist.linear.x, twist_msg->twist.linear.y, twist_msg->twist.linear.z,
+      twist_msg->twist.angular.x, twist_msg->twist.angular.y, twist_msg->twist.angular.z);
 
-            twist_msg->header.frame_id = frame_to_publish_;
-            twist_msg->header.stamp = this->now();
-            twist_pub_->publish(std::move(twist_msg));
-            RCLCPP_DEBUG(this->get_logger(), "Published TwistStamped command to %s", TWIST_TOPIC.c_str());
-        }
-        else
-        {
-            RCLCPP_INFO(this->get_logger(), "Publishing joint velocity command with %zu velocities in frame: %s",
-                        joint_msg->velocities.size(), frame_to_publish_.c_str());
+    twist_msg->header.frame_id = frame_to_publish_;
+    twist_msg->header.stamp = this->now();
+    twist_pub_->publish(std::move(twist_msg));
+    RCLCPP_DEBUG(this->get_logger(), "Published TwistStamped command to %s", TWIST_TOPIC.c_str());
+  } else {
+    RCLCPP_INFO(
+      this->get_logger(), "Publishing joint velocity command with %zu velocities in frame: %s",
+      joint_msg->velocities.size(), frame_to_publish_.c_str());
 
-            joint_msg->header.stamp = this->now();
-            joint_msg->header.frame_id = frame_to_publish_;
-            joint_pub_->publish(std::move(joint_msg));
-            RCLCPP_DEBUG(this->get_logger(), "Published JointJog command to %s", JOINT_TOPIC.c_str());
-        }
+    joint_msg->header.stamp = this->now();
+    joint_msg->header.frame_id = frame_to_publish_;
+    joint_pub_->publish(std::move(joint_msg));
+    RCLCPP_DEBUG(this->get_logger(), "Published JointJog command to %s", JOINT_TOPIC.c_str());
+  }
 
-    } // namespace moveit_servo
+}     // namespace moveit_servo
 }
 
 #include <rclcpp_components/register_node_macro.hpp>
