7815038beef7e3ee36bcdcb272c94bf6b5691ca0
nimiCurtis <nimicu21@gmail.com>
2025-09-10 18:26:03 +0300

    init docs with sphinx rtd

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,9 @@ project = 'SO101 ROS2'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
